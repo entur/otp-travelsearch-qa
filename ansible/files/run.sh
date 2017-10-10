7bd131e7da33c5d639ae65022efe81d018eb5a1b
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-10-10 16:00:27 +0200

    Mute curl

--- ansible/files/run.sh
+++ ansible/files/run.sh
@@ -6,6 +6,6 @@
 python ./graphqltest.py ${ENDPOINTS_FILE} ${UPLOAD_GCP}
 
 echo "Deleting my own pod $(hostname)"
-curl -XDELETE "http://babylon/babylon/api/pod?name=$(hostname)"
+curl --write-out '%{http_code}' --silent --output /dev/null -XDELETE "http://babylon/babylon/api/pod?name=$(hostname)"
 
 echo "done"
