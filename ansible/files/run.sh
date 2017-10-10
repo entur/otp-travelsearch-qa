d6ec52317ed157648316e00953a11b4bd71e5bba
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-10-10 13:00:54 +0200

    Sleep 1m after running docker image for debugging

--- ansible/files/run.sh
+++ ansible/files/run.sh
@@ -7,3 +7,6 @@ python ./graphqltest.py ${ENDPOINTS_FILE} ${UPLOAD_GCP}
 
 echo "Deleting my own pod $(hostname)"
 curl -XDELETE "http://babylon/babylon/api/pod?name=$(hostname)"
+
+echo "sleeping"
+sleep 1m
