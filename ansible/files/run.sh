fa81b8a31e3abae6cbc28b04d639baa1d9cadda2
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-10-27 15:04:12 +0200

    Export notify hubot for python

--- ansible/files/run.sh
+++ ansible/files/run.sh
@@ -2,7 +2,7 @@
 
 : ${ENDPOINTS_FILE="endpoints.csv"}
 : ${UPLOAD_GCP="True"}
-: ${NOTIFY_HUBOT="True"}
+export NOTIFY_HUBOT=${NOTIFY_HUBOT="True"}
 
 python ./graphqltest.py ${ENDPOINTS_FILE} ${UPLOAD_GCP}
 
