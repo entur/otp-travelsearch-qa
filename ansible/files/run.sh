15a3bffc160bae82ad7bc91bed30cb0594b1c78e
Cristoffer Solem <cristoffer.solem@gmail.com>
2017-12-01 14:00:53 +0100

    NRP-2388 Enable testing stop times

--- ansible/files/run.sh
+++ ansible/files/run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 : ${ENDPOINTS_FILE="endpoints.csv"}
+: ${STOP_TIMES_FILE="stop_times.csv"}
 
 export NOTIFY_HUBOT=${NOTIFY_HUBOT="True"}
 export GRAPHITE_REPORT_HOST=${GRAPHITE_REPORT_HOST="graphite"}
 
-python ./graphql_test.py ${ENDPOINTS_FILE}
+python ./graphql_test.py ${ENDPOINTS_FILE} ${STOP_TIMES_FILE}
 
 echo "Deleting my own pod $(hostname)"
 curl --write-out '%{http_code}' --silent --output /dev/null -XDELETE "http://babylon/services/local/deployment/pod?name=$(hostname)"
