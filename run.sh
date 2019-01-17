2869cee273371b9c85cbce2bdd10de00456fe8dc
Recep Cetin <recep.cetin@outlook.com>
2019-01-17 13:21:56 +0100

    remove babylon command to remove current pod- running this program as cronjob

--- run.sh
+++ run.sh
@@ -4,7 +4,4 @@ export GRAPHITE_REPORT_HOST=${GRAPHITE_REPORT_HOST="graphite"}
 
 python ./graphql_test.py ${ENDPOINTS_FILE} ${STOP_TIMES_FILE}
 
-echo "Deleting my own pod $(hostname)"
-curl --write-out '%{http_code}' --silent --output /dev/null -XDELETE "http://babylon/services/local/deployment/pod?name=$(hostname)"
-
 echo "done"
