a16ac79d50b9213ed69d55e17f2075bcff066dfe
Gard Mellemstrand <gard.mellemstrand@entur.org>
2019-03-11 10:10:29 +0100

    Add 10s sleep to run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "Sleeping for 10 seconds..."
+
+sleep 10s
+
 export GRAPHITE_REPORT_HOST=${GRAPHITE_REPORT_HOST="graphite"}
 
 python ./graphql_test.py ${ENDPOINTS_FILE} ${STOP_TIMES_FILE}
